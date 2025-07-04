8f64f898dff0d7648548b3da68ac29dc0cdf568f
rasintrovert <abigaellejean87@gmail.com>
2025-07-04 00:40:15 -0400

    Refactor App component to implement routing with React Router and remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+import Accueil from "./pages/Accueil";
+import APropos from "./pages/APropos";
+import Projets from "./pages/Projets";
+import Idees from "./pages/Idees";
+import Ecriture from "./pages/Ecriture";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Accueil />} />
+      <Route path="/a-propos" element={<APropos />} />
+      <Route path="/projets" element={<Projets />} />
+      <Route path="/idees" element={<Idees />} />
+      <Route path="/ecriture" element={<Ecriture />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
